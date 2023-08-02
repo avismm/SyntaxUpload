9268b8bfd2e6c23f40be0cad42d346304196d731
avismm <amuhwerane@gmail.com>
2023-08-02 23:51:56 +0700

    Addition of two numbers in my first howework

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int num1 =5;
+        int num2=9;
+        int sum=num1+num2;
+        System.out.println(sum);
+
+    }
+}
